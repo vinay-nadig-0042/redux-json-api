3909da41c741cb115e008d488c8f44b2ca6bba0b
A N <nadvinay@u40b0340a44525a2fd449.ant.amazon.com>
2019-04-25 19:49:15 +0530

    Removed keykey as a dependency

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,12 @@
-import keykey from 'keykey';
-
 // Action types of the library
-export default keykey([
-  'API_SET_AXIOS_CONFIG',
-  'API_HYDRATE',
+const actionTypes = {};
+
+// Format the elements
+[
+  'API_SET_ENDPOINT_HOST',
+  'API_SET_ENDPOINT_PATH',
+  'API_SET_HEADERS',
+  'API_SET_HEADER',
   'API_WILL_CREATE',
   'API_CREATED',
   'API_CREATE_FAILED',
@@ -16,4 +19,6 @@ export default keykey([
   'API_WILL_DELETE',
   'API_DELETED',
   'API_DELETE_FAILED'
-]);
+].forEach(action => { actionTypes[action] = action; });
+
+export default actionTypes;
